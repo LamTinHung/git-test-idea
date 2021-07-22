88e64621f2d25ad061f6aa8077b4a8cc7aaea5c2
Lam <lueluelue@nono.com>
2021-07-22 14:56:17 +0800

    second commit

--- src/main/java/com/lalala/git/GitTest.java
+++ src/main/java/com/lalala/git/GitTest.java
@@ -3,5 +3,6 @@ package com.lalala.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git-test-demo");
+        System.out.println("Chase undetected");
     }
 }
