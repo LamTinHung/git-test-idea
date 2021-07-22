88f118ca5c3f074a67f9c6f344c984145c2d340b
Lam <lueluelue@nono.com>
2021-07-22 14:58:00 +0800

    third commit

--- src/main/java/com/lalala/git/GitTest.java
+++ src/main/java/com/lalala/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git-test-demo");
         System.out.println("Chase undetected");
+        System.out.println("Try another");
     }
 }
